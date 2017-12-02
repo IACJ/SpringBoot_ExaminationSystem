d4dfebb97c92fc29a9cab4980cb574105b4b9309
IACJ <iacjacj@qq.com>
2017-12-02 13:38:59 +0800

    燕榕：界面全面升级

--- src/main/java/examination/controller/StudentController.java
+++ src/main/java/examination/controller/StudentController.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
-    final static String path = "student/";
+    final static String path = "/student/";
 
     @Autowired
     ExerciseService exerciseService;
